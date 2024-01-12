6d293aac0d93c69d3d4df944bae5dfd1485e92e2
LilAguh <aguezeochoa@gmail.com>
2024-01-12 05:19:26 -0300

    images collection

---
+++ constants/weatherImages.js
@@ -0,0 +1,19 @@
+export const weatherImages = {
+  'Partly cloudy': require('../assets/images/partlycloudy.png'),
+  'Moderate rain': require('../assets/images/moderaterain.png'),
+  'Patchy rain possible': require('../assets/images/lightrain.png'),
+  'Patchy light rain': require('../assets/images/lightrain.png'),
+  'Sunny': require('../assets/images/sunny.png'),
+  'Clear': require('../assets/images/clear.png'),
+  'Overcast': require('../assets/images/overcast.png'),
+  'Cloudy': require('../assets/images/cloudy.png'),
+  'Light rain': require('../assets/images/lightrain.png'),
+  'Moderate rain at times': require('../assets/images/moderaterainattimes.png'),
+  'Heavy rain': require('../assets/images/heavyrain.png'),
+  'Heavy rain at times': require('../assets/images/heavyrainattimes.png'),
+  'Moderate or heavy freezing rain': require('../assets/images/moderateorheavyfreezingrain.png'),
+  'Moderate or heavy rain shower': require('../assets/images/moderateorheavyrainshower.png'),
+  'Moderate or heavy rain with thunder': require('../assets/images/moderateorheavyrainwiththunder.png'),
+  'Mist': require('../assets/images/mist.png'),
+  'other': require('../assets/images/moderaterain.png')
+}
